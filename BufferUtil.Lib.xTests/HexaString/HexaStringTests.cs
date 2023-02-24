2960f7fb51fa132fcecfd9e256fad1ec09a94cf6
BRAINSHARK\ftorres <frederic.torres@bigtincan.com>
2023-02-24 08:55:15 -0500

    New class HexaString

---
+++ BufferUtil.Lib.xTests/HexaString/HexaStringTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace BufferUtil.Lib.xTests.BufferUtils
+{
+    public class HexaStringTests : BaseTestClass
+    {
+        [Fact]
+        public void Parse()
+        {
+            var result = HexaString.Parse("0001020A");
+            var expected = new List<byte> { 0, 1, 2, 10 };
+            base.AssertBuffer(expected, result);
+        }
+
+
+        [Fact]
+        public void Parse__WithInvalidLength()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                HexaString.Parse("0001020")
+            );
+        }
+
+        [Fact]
+        public void Parse__WithInvalidData()
+        {
+            Assert.Throws<FormatException>(() =>
+                HexaString.Parse("0001020Z")
+            );
+        }
+    }
+}
---
+++ BufferUtil.Lib/BufferUtil.Lib/HexaString/HexaString.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BufferUtil.Lib
+{
+    public class HexaString
+    {
+
+        /// <summary>
+        /// A hexa string is bb0177af
+        /// </summary>
+        public static string ConvertTo(byte[] encrypted)
+        {
+            var result = "";
+
+            foreach (var b in encrypted)
+                result += $"{b.ToString("x")}";
+
+            return result;
+        }
+
+        /// <summary>
+        /// A hexa string is bb0177af
+        /// </summary>
+        /// <param name="hexaString"></param>
+        /// <returns></returns>
+        public static List<byte> Parse(string hexaString)
+        {
+            if (hexaString.Length % 2 == 0)
+            {
+                var r = new List<byte>();
+                var chars = hexaString.ToCharArray().ToList();
+                while (true)
+                {
+                    if (chars.Count > 0)
+                    {
+                        var c0 = chars[0]; chars.RemoveAt(0);
+                        var c1 = chars[0]; chars.RemoveAt(0);
+                        var hexaValue = c0.ToString() + c1.ToString();
+                        var val = Convert.ToByte(hexaValue, 16);
+                        r.Add(val);
+                    }
+                    else break;
+                }
+                return r;
+            }
+            else throw new ArgumentException($"Cannot parse hexa string wrong number of character {hexaString.Length}");
+        }
+    }
+}
